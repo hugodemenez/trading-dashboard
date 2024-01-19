a5d4afae8677edea6d86a9c5c7de9ce8d000af40
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-01-19 21:17:17 +0100

    bug: dvh issue on ipad

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(
-        "min-h-screen bg-background font-sans antialiased dark:dark h-dvh grid grid-rows-[auto,1fr]",
+        "min-h-dvh bg-background font-sans antialiased dark:dark h-dvh grid grid-rows-[auto,1fr]",
         fontSans.variable
       )}
       >
